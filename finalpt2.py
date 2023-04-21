e661c37ec4e7d31f8a1a0bef94b4e9a87e2981b7
saeed357889 <ssamhouri.25@acsamman.edu.jo>
2023-04-21 23:19:28 +0300

    created dictionary of all workouts

---
+++ finalpt2.py
@@ -0,0 +1,11 @@
+workouts = {
+    "Arms": ["Pull-ups", "Partial reps","Dip","Seated hammer curl","Barbell curl","Diamond push up", "Bench press","Power clean","Push-up","Explosive push-ups","Medicine ball throw","Alternating Dumbbell Bench Press","One-arm dumbbell bench press","High pull", "Kettlebell Turkish get-up lunge style","Plank shoulder taps"],
+    "Shoulders" :["Barbell Overhead Press","Dumbbell Overhead Press","Landmine Press","Arnold Press","Bottoms-Up Kettlebell Press","Banded Delt Raise"],
+    "Chest": ["Flat Bench Barbell Press", "Flat Bench Dumbbell Press","Incline Bench Barbell Press","Decline Bench Barbell Press","Decline Bench Dumbbell Press","Decline Bench Dumbbell Fly", "Flat Bench Dumbbell Fly","Dips","Push-up"],
+    "Legs": ["BACK SQUATS ", "Front squats ","Romanian deadlifts ","Good mornings","walking lunges","Reverse lunge ", "Lateral lunge ","Hip thrusts","Bulgarian split squat ","Single leg deadlifts "],
+    "Cardio": ["Jumping rope", "Sprints", "Stair running", "Hill sprints", "Stationary bike"],
+    "CORE": ["Plank", "Russian twist", "Bicycle crunches", "Swiss ball crunches", "Hanging leg raises", "Dead Bug", "Side Plank", "Reverse Crunch", "Medicine Ball Slams", "Windshield Wipers"],
+    "PLYOMETRICS": ["Jumping jacks", "Jump squats", "Tuck jumps", "Box jumps", "Lateral bounds", "Single-leg bounds", "Depth jumps", "Skater jumps", "Split squat jumps", "Plyometric push-ups", "Burpees", "Medicine ball slams", "Cone jumps", "Dot drills", "High knees"]
+}
+workouts["Arms"]
+print(workouts["Shoulders"][3])
